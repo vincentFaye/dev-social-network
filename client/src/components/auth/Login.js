e5eee4e4238022b1015eedccd5d74ebf93323208
VINCE-CTS <47523234+VINCE-CTS@users.noreply.github.com>
2020-02-27 10:06:37 +0000

    WIP : layout the login & the register form

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,46 @@
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+  const [formData, setFormDate] = useState({
+    email: '',
+    password: ''
+  })
+
+  const { email, password } = formData
+
+  const onChange = (e) => setFormDate({ ...formData, [e.target.name]: e.target.value })
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+  }
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" value={email}
+          onChange={e => onChange(e)} />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
+  )
+}
+
+export default Login
